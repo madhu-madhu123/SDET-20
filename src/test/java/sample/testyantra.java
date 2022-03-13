75d9da5f1e948c84d4ddd2af981f3417e02b36b5
ADMIN <ADMIN@DESKTOP-7CF6N7D>
2022-03-13 16:57:56 +0530

    commit tc-50 and tc_20

---
+++ src/test/java/sample/testyantra.java
@@ -0,0 +1,12 @@
+package sample;
+
+import org.testng.annotations.Test;
+
+public class testyantra 
+{
+   @Test
+   public void sample()
+   {
+	   System.out.println(" new added test case");
+   }
+}
